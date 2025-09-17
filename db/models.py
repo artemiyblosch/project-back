6df835907270a1ddaa038f4b6199c2095c6d16a1
arti <arti@privategatevpn.com>
2025-09-17 23:32:27 +0300

    Added complimentary methods

--- db/models.py
+++ db/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 
+class User(models.Model):
+    name = models.CharField(max_length=64)
+    tag = models.CharField(max_length=32)
+    groups = models.ManyToManyField("Group")
+
+    def __str__(self):
+        return f"@{self.tag}"
+
 class Group(models.Model):
     name = models.CharField(max_length=64)
     members = models.ManyToManyField("User")
 
-class User(models.Model):
-    name = models.CharField(max_length=64)
-    tag = models.CharField(max_length=32)
-    groups = models.ManyToManyField(Group)
+    def add_member(self,user : User):
+        user.groups.add(self)
+        self.members.add(user)
+
+    def __str__(self):
+        return f"{self.name}"
